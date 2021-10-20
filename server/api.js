e60b47a2b83838a4447a84272c8217e7e41325c6
Anthony Johnson <anthonythomasjohnson@outlook.com>
2021-10-20 18:05:50 +0900

    created modules and import and mount routers

--- server/api.js
+++ server/api.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const app = require('../server');
 const apiRouter = express.Router();
 
+// Import routers
+const ideasRouter = require('./ideas.js');
+const meetingsRouter = require('./meetings.js');
+const minionsRouter = require('./minions.js');
 
+// Mount routers
+app.use('/ideas', ideasRouter);
+app.use('/meetings', meetingsRouter);
+app.use('/minions', minionsRouter);
 
 module.exports = apiRouter;
